87eca9f8767566d9fb938bb146c78377c985fcb6
AnonimoLopez <jose_lopez_emo@hotmail.com>
2016-11-28 00:56:17 -0600

    primero

---
+++ src/examen/pizza.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen;
+
+/**
+ *
+ * @author anonimo
+ */
+public class pizza {
+    int size;
+    String ingredientes="";
+    double price;
+
+    public int getSize() {
+        return size;
+    }
+ 
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
